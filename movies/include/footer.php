aad9352b29539e87674cf0d12ceaced7c3eed5ee
MORO-66t <scwtud@gmail.com>
2024-05-04 16:54:18 +0300

    add new temp

---
+++ movies/include/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+			</div>
+			<div class="copy-rights text-center">
+				<p>&copy; 2019 Easy Movie Booking. All Rights Reserved | Design by <a href="">Pdwap Tech</a></p>
+			</div>
+	</div>
+</body>
+</html>
